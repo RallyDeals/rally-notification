97f886b6cd0d60b4014eb0b3f6ae1416e924a174
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2026-08-09 21:33:07 +0300

    init commit

---
+++ src/main/java/com/rally/notification/RallyNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.rally.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RallyNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RallyNotificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rally/notification/RallyNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rally.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RallyNotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
